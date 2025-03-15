c3de08dcf57c6a25524cea963a8cc37f88b2cb24
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-03-15 16:34:12 +1300

    update app to show Layout

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,11 +1,9 @@
+import Layout from "./Layout"
+
 function App() {
 
   return (
-    <>
-      <div className="app">
-        <h1>Cafe Sim App comp</h1>
-      </div>
-    </>
+    <Layout />
   )
 }
 
