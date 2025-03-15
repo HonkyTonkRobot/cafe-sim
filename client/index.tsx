1ad48e6b66eb30e7b4e581faf05fa0cb1f895182
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-03-15 15:21:34 +1300

    add index.tsx boiler plate for rendering the App component

--- client/index.tsx
+++ client/index.tsx
@@ -1,19 +1,7 @@
 import { createRoot } from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-import routes from './routes.tsx'
-
-const queryClient = new QueryClient()
-
-const router = createBrowserRouter(routes)
+import App from './components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
-  createRoot(document.getElementById('app') as HTMLElement).render(
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools />
-    </QueryClientProvider>,
-  )
+  createRoot(document.getElementById('app') as HTMLElement).render(<App />)
 })
