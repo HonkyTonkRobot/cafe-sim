ef2134d4e5e79da3c2ec5c075d63dcc396f99f19
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-03-20 10:54:12 +1300

    minor header and footer nav css adjustments

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -8,18 +8,11 @@ function Footer() {
     <>
       <footer className={`container ${css.footer}`}>
         <nav>
-          <ul>
-            <li><Link to="/store">Store</Link></li>
-          </ul>
-          <ul>
-            <li><Link to="/tickets">Orders</Link></li>
-          </ul>
-          <ul>
-            <li><Link to="/register">Register</Link></li>
-          </ul>
-          <ul>
-            <li><Link to="/menu">Menu</Link></li>
-          </ul>
+          {/* html structure is like this to follow pico css rules */}
+          <ul><li><Link to="/store">Store</Link></li></ul>
+          <ul><li><Link to="/tickets">Orders</Link></li></ul>
+          <ul><li><Link to="/register">Register</Link></li></ul>
+          <ul><li><Link to="/menu">Menu</Link></li></ul>
         </nav>
       </footer >
     </>
