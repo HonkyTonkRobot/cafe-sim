c75d93a3f377a83c5b6ce7050eeb6965e7c80d49
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-03-20 14:16:19 +1300

    make menu have section for drinks and food

--- client/components/Menu.tsx
+++ client/components/Menu.tsx
@@ -6,12 +6,20 @@ function Menu() {
     <>
       <article className={style.content}>
         <h1>Menu</h1>
-        {/* TODO: Replace with an array of objects in a data file and map over them */}
-        <p>Black Coffee</p>
-        <p>White Coffee</p>
-        <p>Chai</p>
-        <p>Hot Chocolate</p>
-        <p>Green tea</p>
+        <div className='grid'>
+          <div>
+            <h3>Drinks</h3>
+            {/* TODO: Replace with an array of objects in a data file and map over them */}
+            <p>Black Coffee</p>
+            <p>White Coffee</p>
+            <p>Chai</p>
+            <p>Hot Chocolate</p>
+            <p>Green tea</p>
+          </div>
+          <div>
+            <h3>Food</h3>
+          </div>
+        </div>
       </article>
     </>
   )
