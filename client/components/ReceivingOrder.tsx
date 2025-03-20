17d5ee943825e3adae6ac4d1c37a256ad0e812f1
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-03-20 14:15:36 +1300

    create order complete/deliver to customer modal

--- client/components/ReceivingOrder.tsx
+++ client/components/ReceivingOrder.tsx
@@ -1,12 +1,35 @@
+import { useRef } from "react"
 import '../../pico.sand.css'
+import style from "../styles/customerModal.module.css"
 
 function ReceivingOrder() {
+  const dialogRef = useRef<HTMLDialogElement | null>(null)
+
+  const openDialog = () => {
+    if (dialogRef.current) {
+      dialogRef.current.showModal()
+    }
+  }
+
+  const closeDialog = () => {
+    if (dialogRef.current) {
+      dialogRef.current.close()
+    }
+  }
+
   return (
     <>
-      <h1>Receiving order screen</h1>
+      <button className={style.navLink} onClick={openDialog}>Order Complete</button>
+
+      <dialog ref={dialogRef}>
+        <article className={style.modal}>
+
+          <p>Thank you, it looks delicious...</p>
+          <a onClick={closeDialog}>close</a>
+        </article>
+      </dialog>
     </>
   )
 }
 
 export default ReceivingOrder
-
--- client/components/Tickets.tsx
+++ client/components/Tickets.tsx
@@ -1,11 +1,14 @@
 import '../../pico.sand.css'
 import style from '../styles/content.module.css'
+import CustomerOrder from './CustomerOrder'
+import ReceivingOrder from './ReceivingOrder'
 
 function Tickets() {
   return (
     <>
       <article className={style.content}>
         <h1>Orders</h1>
+        <ReceivingOrder />
       </article>
     </>
   )
